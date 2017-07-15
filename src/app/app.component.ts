e8c3c4e6a35fc202d39ef96267d855937b4bb642
Ayush Goyal <ayush987goyal@gmail.com>
2017-07-15 18:24:23 +0530

    Final workign commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Marked from 'marked';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  inputText = '';
+  outputText: string = '';
+
+  ngOnInit() {
+    Marked.setOptions({
+      renderer: new Marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false
+    });
+  }
+
+  makeMarked(value) {
+    this.outputText = Marked(value);
+  }
+
 }
